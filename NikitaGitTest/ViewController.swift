6255298bb443ebfdd9130fb20af0f97958790f53
Nikita Zharinov <nikita.zharinov@gmail.com>
2021-02-20 16:53:58 +0100

    Initial Commit

---
+++ NikitaGitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NikitaGitTest
+//
+//  Created by Nikita Zharinov on 20/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NikitaGitTest/fiel_1.swift
@@ -0,0 +1,8 @@
+//
+//  fiel_1.swift
+//  NikitaGitTest
+//
+//  Created by Nikita Zharinov on 20/02/2021.
+//
+
+import Foundation
